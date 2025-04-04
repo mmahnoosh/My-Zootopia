7b0351db26b8a488528a91d3f3454e7b172b9697
mmahnoosh <montazer_mahnoosh@gmx.de>
2025-04-04 08:11:34 +0200

    “improve card item html”

--- animal_funktions.py
+++ animal_funktions.py
@@ -1,10 +1,45 @@
+import json
+
+from animals_web_generator import animals_data
+
+
 def save_animals(animals):
     """
     Gets all your animals as an argument and saves them to the html file.
     """
     try:
         with open("animals.json", "w", encoding="utf-8") as file:
-            json.dump(movies, file, indent=4)
-        print("Movies successfully saved!")
+            json.dump(animals_data, file, indent=4)
+        print("Animal successfully saved!")
     except IOError:
-        print("Error: Unable to save the movies.")
+        print("Error: Unable to save the animal.")
+
+def load_data(file_path):
+    """ Loads a JSON file """
+    with open(file_path, "r") as handle:
+        return json.load(handle)
+
+
+def create_html():
+    animals_dataset = load_data('animals_data.json')
+    html_template = load_data('animals_template.html')
+    output = []
+    animal_entries = []
+    for animal in animals_dataset:
+        animal_entries.append(f"""
+            <li class="cards__item">
+                <div class="card__title">{animal['name']}</div>
+                <p class="card__text">
+                    <strong>Diet:</strong> {animal['characteristics']['diet']}<br/>
+                    <strong>Location:</strong> {animal['characteristics']['location']}<br/>
+                    <strong>Type:</strong> {animal['characteristics']['type']}<br/>
+                </p>
+            </li>
+        """)
+    animals_html = "\n".join(output)
+    with open('animals_template.html', 'r', encoding='utf-8') as f:
+        html_template = f.read()
+
+    final_html = html_template.replace("__REPLACE_ANIMALS_INFO__", animals_html)
+    with open('animals.html', 'w', encoding='utf-8') as f:
+        f.write(final_html)
--- animals_web_generator.py
+++ animals_web_generator.py
@@ -3,8 +3,36 @@ import json
 
 def load_data(file_path):
     """ Loads a JSON file """
-    with open(file_path, "r") as handle:
+    with open(file_path, "r", encoding="utf-8") as handle:
         return json.load(handle)
+
+
+animals_dataset = load_data('animals_data.json')
+
+animal_entries = []
+for animal in animals_dataset:
+    locations = ", ".join(animal['locations'])
+    animal_type = animal['characteristics'].get('type', '-')
+    animal_entries.append(f"""
+            <li class="cards__item">
+                <div class="card__title">{animal['name']}</div>
+                <p class="card__text">
+                    <strong>Diet:</strong> {animal['characteristics']['diet']}<br/>
+                    <strong>Location:</strong> {locations}<br/>
+                    <strong>Type:</strong> {animal_type}<br/>
+                </p>
+            </li>
+        """)
+animals_html = "\n".join(animal_entries)
+with open('animals_template.html', 'r', encoding='utf-8') as f:
+    html_template = f.read()
+
+final_html = html_template.replace("__REPLACE_ANIMALS_INFO__", animals_html)
+with open('animals.html', 'w', encoding='utf-8') as f:
+    f.write(final_html)
+
+"""
+animal_funktions.create_html()
 
 animals_data = load_data('animals_data.json')
 output = ""
@@ -23,11 +51,11 @@ for animal in animals_data:
         output += f"Type: {animal['characteristics']['type']}<br/>"
     else:
         output += "-<br/>"
-    output += '</li>\n    '
+    output += '</li>   '
 
 with open('animals_template.html', 'r', encoding='utf-8') as f:
     html_template = f.read()
 
 final_html = html_template.replace("__REPLACE_ANIMALS_INFO__", output)
 with open('animals.html', 'w', encoding='utf-8') as f:
-    f.write(final_html)
+    f.write(final_html)"""
